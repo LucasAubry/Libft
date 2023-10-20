728f426efeb2f13c8342b13b5332e2a8a6210339
Lucas Aubry <laubry@c1r6p10.42lehavre.fr>
2023-10-21 00:05:23 +0200

    oui

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,16 +6,9 @@
 /*   By: Laubry <aubrylucas.pro@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 01:44:59 by Laubry            #+#    #+#             */
-/*   Updated: 2023/10/20 18:23:10 by Laubry           ###   ########.fr       */
+/*   Updated: 2023/10/21 00:04:00 by Laubry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-copier src dans dest 
-
-pour ca faut calculer la taille de dest ce mtre
- a la fin du mot et incrementer 
-pour print src dans dest 
-
 
 #include "libft.h"
 
@@ -26,8 +19,8 @@ size_t ft_strlcat(char *dest, const char *src, size_t size)
 	int i;
 
 	i = 0;
-	tdest = ft_strlen(dest)
-	tsrc = ft_strlen(src)
+	tdest = ft_strlen(dest);
+	tsrc = ft_strlen(src);
 	if (size == 0)
 		return (tsrc);
 	while (src[i] && tdest + i < (size -1))
@@ -38,6 +31,6 @@ size_t ft_strlcat(char *dest, const char *src, size_t size)
 	if (i < size)
 		dest[i + tdest] = '\0';
 	if (tdest > size)
-		return (tsrc + size)
+		return (tsrc + size);
 	return (tsrc + tdest);
 }
